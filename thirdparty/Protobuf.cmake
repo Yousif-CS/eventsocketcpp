f7dbb0e7fc92d56662ea9761788568b4cd1efc6d
Yousif <yousef2000khalid@gmail.com>
2020-12-15 00:22:12 +1100

    added protobuf as a downloaded dependency

---
+++ thirdparty/Protobuf.cmake
@@ -0,0 +1,21 @@
+# Installing and linking protobuf to the current project
+
+cmake_minimum_required(VERSION 3.11)
+
+
+# using FetchContent
+include(FetchContent)
+
+
+FetchContent_Declare (
+    protobuf
+    GIT_REPOSITORY https://github.com/protocolbuffers/protobuf
+    GIT_TAG        v3.14.0
+    SOURCE_SUBDIR       cmake
+)
+
+FetchContent_GetProperties(protobuf)
+
+if (NOT protobuf_POPULATED)
+    FetchContent_MakeAvailable(protobuf)
+endif()
